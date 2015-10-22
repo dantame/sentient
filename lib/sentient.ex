4088e6ce9e1163d0b2549fe63f56d3b6ba38dfd3
Dan Tame <dantame89@gmail.com>
2015-10-22 21:26:04 +0100

    Use more pipes!

--- lib/sentient.ex
+++ lib/sentient.ex
@@ -1,8 +1,9 @@
 defmodule Sentient do
   def analyze(phrase, override_words \\ %{}) do
     tokenized_phrase = Tokenizer.tokenize(phrase)
-    afinn_source = File.read!(__DIR__ <> "/../config/AFINN-111.json")
-    afinn = Poison.Parser.parse!(afinn_source) |> Map.merge(override_words)
+    afinn = File.read!(__DIR__ <> "/../config/AFINN-111.json")
+            |> Poison.Parser.parse!
+            |> Map.merge(override_words)
 
     tokenized_phrase
     |> Enum.filter(fn(word) -> afinn[word] end)
