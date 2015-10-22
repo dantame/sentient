ba5617a544eb4656faa906c9a4e7fa41e572b4ea
Dan Tame <dantame89@gmail.com>
2015-10-22 19:59:24 +0100

    Add data for packaging to hex

--- mix.exs
+++ mix.exs
@@ -5,27 +5,16 @@ defmodule Sentient.Mixfile do
     [app: :sentient,
      version: "0.0.1",
      elixir: "~> 1.1",
+     description: "Simple sentiment analysis based on the AFINN-111 wordlist",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:poison, "~> 1.5"}]
   end
