4f862f7d1f5bdd1aab3d7fdb602f022a3c138d63
Tame, Dan <Dan.Tame@sky.uk>
2015-11-11 11:39:18 +0000

    Prevent errors

--- lib/sentient.ex
+++ lib/sentient.ex
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule Sentient do
   @afinn_source File.read!(__DIR__ <> "/../config/AFINN-111.json")
                 |> Poison.Parser.parse!
@@ -7,9 +9,11 @@ defmodule Sentient do
             |> Map.merge(override_words)
 
     phrase
+    |> String.replace(~r/\n/, "")
     |> Tokenizer.tokenize
     |> Enum.filter(&(afinn[&1]))
     |> Enum.map(&(afinn[&1]))
-    |> Enum.reduce(&(&1 + &2))
+    |> Enum.reduce(0, &(&1 + &2))
+
   end
 end
